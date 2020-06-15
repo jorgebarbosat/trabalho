228c08a9a014e03344c4b95aed4868c2e674914e
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-06-15 20:16:08 -0300

    .

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -10,3 +10,9 @@ function Enviar() {
     }
  
 }
+document.getElementById('contatoForm').addEventListener('submit', function () {
+    var nome = this.querySelector('input[name=nome]'), nome = nome.value;
+    var email = this.querySelector('input[name=email]'), email = email.value;
+    var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
+    this.querySelector('input[name=Body]').setAttribute('value', texto);
+});

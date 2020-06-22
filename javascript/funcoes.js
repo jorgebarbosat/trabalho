f652328b2678f881531125aeceafa98024ee3c75
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-06-22 10:31:45 -0300

    .

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -16,3 +16,28 @@ document.getElementById('menu_Form').addEventListener('submit', function () {
     var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
     this.querySelector('input[name=Body]').setAttribute('value', texto);
 });
+$(document).ready(function(){
+    
+    // hide #back-top first
+    $('#backtopo').hide();
+       
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 150) {
+                $('#backtopo').fadeIn();
+            } else {
+                $('#backtopo').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#backtopo').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+});
